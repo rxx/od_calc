e492aca01050daadac3050619ccde0287f3294cf
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-06-09 00:19:31 +0300

    Fix colors, remove info stats

--- src/components/JsonArea.tsx
+++ src/components/JsonArea.tsx
@@ -5,41 +5,39 @@ import { useState } from 'react';
 function JsonArea() {
   const stats = useStore($stats);
   const name: string = stats?.status?.name || 'dominion name'
-  const [jsonValue, setValue] = useState('')
+  const [inputVisible, setVisibility] = useState(!name);
 
   const setStats = (json: string) => {
-    setValue(json);
-
     if (!json) {
       $stats.set(null);
+      setVisibility(true);
     }
 
-    if (json) {
-      try {
-        const parsedData = JSON.parse(json);
-        $stats.set(parsedData);
-      } catch (err) {
-        console.error("Error on parsing json:", err);
-      }
+    try {
+      const parsedData = JSON.parse(json);
+      setVisibility(false);
+      $stats.set(parsedData);
+    } catch (err) {
+      console.error("Error on parsing json:", err);
     }
   }
 
   return (
     <div className="grid grid-cols-2 gap-2">
-      <div className="col-span-full rounded-md bg-gray-800 p-2 text-center
+      <div className="rounded-md bg-gray-800 p-2 text-center
         align-middle font-bold capitalize text-indigo-400 shadow-md">
         {name}
       </div>
-      <textarea className="h-10 rounded-md bg-gray-800 p-2 text-white"
-        value={jsonValue} placeholder="Put ops here" spellCheck="false"
+      {inputVisible && <textarea className="h-10 rounded-md bg-gray-800 p-2 text-white"
+        placeholder="Put ops here" spellCheck="false"
         onChange={(e) => setStats(e.target.value)}
-      />
+      />}
 
-      <button className="h-10 rounded-md bg-gray-600 text-white
-          hover:bg-gray-700" onClick={() => setStats('')}
+      {!inputVisible && <button className="h-10 rounded-md bg-sky-900 text-white
+          shadow hover:bg-sky-700" onClick={() => setStats('')}
       >
         Clear
-      </button>
+      </button>}
     </div>
   )
 }
